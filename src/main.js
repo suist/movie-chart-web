6580c1f9d8c95103574281774a929a37c849bec3
suist <skoco73@gmail.com>
2020-04-19 17:56:39 +0900

    list-infinite-scroll

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import App from './App.vue'
 import router from './router'
 import  BootstrapVue from 'bootstrap-vue'
 import axios from 'axios'
-import InfiniteLoading from 'vue-infinite-loading'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-Vue.use(InfiniteLoading)
 
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <div style="display:flex; flex-direction:column; justify-content:center">
+      
         <list-card :card="item" v-for="item in movieList" :key="item.id" style="margin:12px 100px;"></list-card>
+        <infinite-loading @infinite="infiniteHandler" force-user-infinite-warpper=".el-table__body-wrapper" slot="append"></infinite-loading>
     </div>
   </div>
 </template>
@@ -9,32 +11,54 @@
 <script>
 
 import ListCard from '@/components/ListCard.vue'
-
+import InfiniteLoading from 'vue-infinite-loading'
 
 export default {
   name: 'Home',
   components: {
 
-    ListCard
+    ListCard,
+     InfiniteLoading
   },
   data() {
     return{
         movieList: [],
         baseUrl: 'https://api.themoviedb.org/3',
         apiKey: 'b1732a7e79da2bd0d0023abb9d6df1f8',
-        loaded: true
+        loaded: true,
+        page:1,
     }},
     created() {
-        this.getData();
+        // this.getData();
     },
     methods: {
 
-        getData() {
-            this.$axios.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc').then(res => {
-                this.movieList = res.data.results;
-                this.loaded = false;
-            });
-        }
+        // getData() {
+        //     this.$axios.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc').then(res => {
+        //         this.movieList = res.data.results;
+        //         this.loaded = false;
+        //     });
+        // },
+        infiniteHandler($state) {
+          this.$axios.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc',{
+          params: {
+            page :this.page,
+          },
+        })
+            .then(res => {
+              setTimeout(() => { //스크롤 페이징을 띄우기 위한 시간 지연(1초)
+                if (res.data.results.length) {
+                  this.page+=1;
+                  this.movieList= this.movieList.concat(res.data.results)
+                  $state.loaded(); //데이터 로딩
+                } else {
+                  $state.complete();
+                }
+              }, 1000)
+            }).catch(error => {
+              console.error(error);
+            })
+            }
     }
 }
 </script>
