af22aef0edd8ed1722babc3433604197fb3e25ae
suist <skoco73@gmail.com>
2020-04-19 17:12:18 +0900

    list-ugly

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -3,15 +3,18 @@
   <b-card no-body class="overflow-hidden" style="max-width: 540px;">
     <b-row no-gutters>
       <b-col md="6">
-        <b-card-img :src="movieData.image" alt="Image" class="rounded-0"></b-card-img>
+        <b-card-img :src="imageUrl + card.poster_path" alt="Image" class="rounded-0"></b-card-img>
       </b-col>
       <b-col md="6">
-        <b-card-body title="Movie title">
+        <b-card-body>
           <b-card-text>
-          Date: {{movieData.date}}
+           {{card.title}}
           </b-card-text>
           <b-card-text>
-           Rate: {{movieData.rate}}
+          Date: {{card.release_date}}
+          </b-card-text>
+          <b-card-text>
+           Rate: {{card.vote_average}} ({{card.vote_count}})
           </b-card-text>
           <b-button :href="movieData.link" variant="primary">Go Detail</b-button>
         </b-card-body>
@@ -27,11 +30,19 @@ export default {
     return {
       movieData:
         {"image":'https://picsum.photos/400/400/?image=20',title:'Movie title',date:'2012-02-02',rate:'4.5',link:'https://naver.com'}
-
+      ,
+      imageUrl: 'https://image.tmdb.org/t/p/w342',
+      
     
 
 
     }
+  },
+  props:{
+    card:{
+      type:Object
+    }
+
   },
   methods: {
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="home">
     <div style="display:flex; flex-direction:column; justify-content:center">
-        <list-card v-for="item in 10" :key="item.id" style="margin:12px 100px;"></list-card>
+        <list-card :card="item" v-for="item in movieList" :key="item.id" style="margin:12px 100px;"></list-card>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 import ListCard from '@/components/ListCard.vue'
+
 
 export default {
   name: 'Home',
   components: {
 
     ListCard
-  }
+  },
+  data() {
+    return{
+        movieList: [],
+        baseUrl: 'https://api.themoviedb.org/3',
+        apiKey: 'b1732a7e79da2bd0d0023abb9d6df1f8',
+        loaded: true
+    }},
+    created() {
+        this.getData();
+    },
+    methods: {
+
+        getData() {
+            this.$axios.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc').then(res => {
+                this.movieList = res.data.results;
+                this.loaded = false;
+            });
+        }
+    }
 }
 </script>
